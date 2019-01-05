7d926a0f3fb152f28c3afc8f5bbbcc54e703a082
bousui <bousui@bg.wakwak.com>
2019-01-05 14:07:10 +0900

    WebPackのサンプルソースを追加

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// メインモジュールのテスト
+document.write('Main module sample.');
+
+// サブモジュールのテスト
+import {sub_method} from './sub-module'
+sub_method();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // エントリーポイント設定
+    entry: './src/main.js',
+
+    // ファイルの出力設定
+    output: {
+        // 出力ディレクトリ名
+        path: `${__dirname}/dist`,
+        // 出力ファイル名
+        filename: 'main.js'
+    },
+
+    // デバッグ・リリースモード設定(development/production)
+    mode: 'development',
+};
+
+// htmlファイルのパスを取得
+const path = require('path');
+const filename = '/index.html';
+const srcpath = path.dirname(module.exports.entry) + filename;
+const distpath = module.exports.output.path + filename;
+
+// ファイルを出力先にコピー
+const fs = require('fs');
+fs.copyFileSync(srcpath, distpath);
