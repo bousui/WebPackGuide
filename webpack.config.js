c682745c5b88fb2c673487fed03b291533928440
bousui <dev.bousui.2010@gmail.com>
2019-01-10 10:40:46 +0900

    copy-webpack-pluginでhtmlファイルをコピーするように設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+// プラグイン読込
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+// WebPack設定
 module.exports = {
     // エントリーポイント設定
     entry: './src/assets/js/main.js',
@@ -46,17 +50,18 @@ module.exports = {
             }],
         }]
     },
+
+    // プラグイン設定
+    plugins: [
+        // copy-webpack-plugin設定
+        new CopyWebpackPlugin([{
+            // コピー元パス
+            from: '*.html',
+            // 出力ディレクトリ基準のコピー先パス
+            to: '../..',
+        }], {
+            // コピー元の基準パス
+            context: './src',
+        }),
+    ],
 };
-
-// htmlファイルのパスを取得
-const path = require('path');
-const filename = '/../../index.html';
-const srcpath = path.dirname(module.exports.entry) + filename;
-const distpath = module.exports.output.path + filename;
-
-// ファイルを出力先にコピー
-const fs = require('fs');
-if (!fs.existsSync(module.exports.output.path)) {
-    fs.mkdirSync(module.exports.output.path);
-}
-fs.copyFileSync(srcpath, distpath);
